f118422acf911c5eabd0925164cdf0fb6b31f3ce
jf908 <josh@thefindons.com>
2023-11-28 12:36:48 +0000

    Register slash commands

--- index.ts
+++ index.ts
@@ -6,9 +6,11 @@ import {
   Collection,
   Events,
   GatewayIntentBits,
+  REST,
+  Routes,
   SlashCommandBuilder,
 } from 'discord.js';
-import { token, broadcastChannelId } from './config.json';
+import { clientId, token, broadcastChannelId } from './config.json';
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -25,11 +27,12 @@ const yesCommand = {
   data: new SlashCommandBuilder()
     .setName('yes')
     .setDescription('Sends YES to chat.'),
-  async execute() {
+  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
     const channel = client.channels.cache.get(broadcastChannelId);
     if (channel?.type === ChannelType.GuildText) {
       await channel.send('YES');
     }
+    interaction.reply('Sent YES to chat.');
   },
 };
 
@@ -37,11 +40,12 @@ const noCommand = {
   data: new SlashCommandBuilder()
     .setName('no')
     .setDescription('Sends NO to chat.'),
-  async execute() {
+  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
     const channel = client.channels.cache.get(broadcastChannelId);
     if (channel?.type === ChannelType.GuildText) {
       await channel.send('NO');
     }
+    interaction.reply('Sent NO to chat.');
   },
 };
 
@@ -49,11 +53,12 @@ const idkCommand = {
   data: new SlashCommandBuilder()
     .setName('idk')
     .setDescription("Sends I DON'T KNOW to chat."),
-  async execute() {
+  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
     const channel = client.channels.cache.get(broadcastChannelId);
     if (channel?.type === ChannelType.GuildText) {
       await channel.send("I DON'T KNOW");
     }
+    interaction.reply("Sent I DON'T KNOW to chat.");
   },
 };
 
@@ -95,3 +100,20 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+(async () => {
+  const rest = new REST().setToken(token);
+  try {
+    const data = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands.map((c) => c.data.toJSON()),
+    });
+
+    console.log(
+      `Successfully reloaded ${
+        (data as any[]).length
+      } application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
